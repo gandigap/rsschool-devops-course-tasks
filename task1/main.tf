a05ee645adcac7412427141526bd4d6bc1ee2f4c
iharberasneu <iberesnev@alfabank.ru>
2024-09-27 08:50:02 +0300

    feat(*): add gha yaml and main tf

---
+++ task1/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create s3 bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket-rs"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
