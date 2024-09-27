5f1a2e80845c60e58ea9b206e00b5815945dd434
iharberasneu <iberesnev@alfabank.ru>
2024-09-27 11:33:00 +0300

    feat(*): start terraform init, plan, apply for create s3 bucket and role

---
+++ main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Create s3 bucket
+resource "aws_s3_bucket" "example" {
+  bucket = "my-test-bucket-rs"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_iam_role" "github_actions_role" {
+  name = "GithubActionsRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      },
+    ]
+  })
+}
+
+resource "aws_iam_policy" "github_actions_policy" {
+  name        = "GithubActionsPolicy"
+  description = "Policy for GitHub Actions with necessary permissions"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "ec2:*",
+          "route53:*",
+          "s3:*",
+          "iam:*",
+          "vpc:*",
+          "sqs:*",
+          "events:*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_policy_attachment" {
+  role       = aws_iam_role.github_actions_role.name
+  policy_arn = aws_iam_policy.github_actions_policy.arn
+}
