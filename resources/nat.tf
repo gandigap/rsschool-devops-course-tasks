df5dbf0ec3ce3b0aa523c61f40e440f2ee3703f5
iharberasneu <iberesnev@alfabank.ru>
2024-10-11 14:26:12 +0300

    feat(*): add temporary commment for nat because this resouce not free

--- resources/nat.tf
+++ resources/nat.tf
@@ -1,26 +1,28 @@
-# Allocate an Elastic IP for the NAT Gateway
-resource "aws_eip" "nat_eip" {
-  domain = "vpc"
+//Отключил чтобы не платить
 
-  tags = {
-    Name    = "Elastic IP for the NAT Gateway"
-    Project = "Rs-devops"
-  }
-}
+# # Allocate an Elastic IP for the NAT Gateway
+# resource "aws_eip" "nat_eip" {
+#   domain = "vpc"
 
-# Create the NAT Gateway
-resource "aws_nat_gateway" "nat_gateway" {
-  allocation_id = aws_eip.nat_eip.id
-  subnet_id     = aws_subnet.public_subnet_a.id
+#   tags = {
+#     Name    = "Elastic IP for the NAT Gateway"
+#     Project = "Rs-devops"
+#   }
+# }
 
-  tags = {
-    Name = "NAT-Gateway"
-  }
-}
+# # Create the NAT Gateway
+# resource "aws_nat_gateway" "nat_gateway" {
+#   allocation_id = aws_eip.nat_eip.id
+#   subnet_id     = aws_subnet.public_subnet_a.id
 
-# Update the route for private subnets
-resource "aws_route" "private_route" {
-  route_table_id         = aws_route_table.aws_private_route_table.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat_gateway.id
-}
+#   tags = {
+#     Name = "NAT-Gateway"
+#   }
+# }
+
+# # Update the route for private subnets
+# resource "aws_route" "private_route" {
+#   route_table_id         = aws_route_table.aws_private_route_table.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   nat_gateway_id         = aws_nat_gateway.nat_gateway.id
+# }
