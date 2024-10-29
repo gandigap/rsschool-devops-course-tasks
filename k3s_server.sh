9c3e107f3a7a023604cbac19a47bae22baaee22a
iharberasneu <iberesnev@alfabank.ru>
2024-10-29 22:44:42 +0300

    feat(*): add jenkins config and screenshots

--- k3s_server.sh
+++ k3s_server.sh
@@ -19,8 +19,11 @@ if command -v yum &> /dev/null && command -v curl &> /dev/null; then
     done
 
     # Вывод kubeconfig
+    sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+    sudo systemctl status k3s
     sudo cat /etc/rancher/k3s/k3s.yaml
-
+    # export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+    
     # Установка Helm
     curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
 
@@ -36,6 +39,57 @@ if command -v yum &> /dev/null && command -v curl &> /dev/null; then
 
     # Удаление Nginx
     helm uninstall my-nginx
+
+    # Создание пространства имён для Jenkins
+    kubectl create namespace jenkins
+
+    # Установка Jenkins с использованием Helm
+    helm repo add jenkins https://charts.jenkins.io
+    helm repo update
+    helm install my-jenkins jenkins/jenkins --namespace jenkins --set controller.debug=true
+
+    # Ожидание запуска Jenkins
+    kubectl rollout status deployment/my-jenkins -n jenkins
+
+    # Проверка успешного развертывания Jenkins
+    kubectl get pods -n jenkins
+
+else
+    echo "yum or curl is not available, aborting."
+fi
+
+#!/bin/bash
+
+while ! systemctl is-active --quiet sshd; do
+    echo "Waiting for SSH service to be active..."
+    sleep 10
+done
+
+if command -v yum &> /dev/null && command -v curl &> /dev/null; then
+    sudo amazon-linux-extras enable selinux-ng
+    sudo yum install selinux-policy-targeted -y
+
+    # Установка K3s
+    curl -sfL https://get.k3s.io | K3S_TOKEN=${token} sh -s -
+
+    # Ожидание доступности kubeconfig
+    while [ ! -f /etc/rancher/k3s/k3s.yaml ]; do
+        echo "Waiting for kubeconfig to be available..."
+        sleep 10
+    done
+
+    # Вывод kubeconfig
+    sudo cat /etc/rancher/k3s/k3s.yaml
+
+    # Установка Helm
+    curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+    # Установка Jenkins
+    helm repo add jenkins https://charts.jenkins.io
+    helm install my-jenkins jenkins/jenkins --namespace jenkins --create-namespace
+
+    # Проверка установки Jenkins
+    kubectl get pods -n jenkins
 
 else
     echo "yum or curl is not available, aborting."
